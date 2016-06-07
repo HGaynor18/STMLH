6b12c175b0f55703f5a180fd5f305375b84ef130
HGaynor18 <harrison26gaynor@gmail.com>
2016-06-07 11:54:47 +1000

    Changes

---
+++ водка Drinker/водка Drinker/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace водка_Drinker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.VodkaPicture = new System.Windows.Forms.PictureBox();
+            this.VodkaCounter = new System.Windows.Forms.Label();
+            this.VodkaButton = new System.Windows.Forms.Button();
+            this.ShopDistiller = new System.Windows.Forms.Button();
+            this.DistillerTimer = new System.Windows.Forms.Timer(this.components);
+            this.DistillerCounterLabel = new System.Windows.Forms.Label();
+            this.DistillerPriceLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.VodkaPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VodkaPicture
+            // 
+            this.VodkaPicture.BackgroundImage = global::водка_Drinker.Properties.Resources.russian_vodka_beluga;
+            this.VodkaPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.VodkaPicture.Location = new System.Drawing.Point(438, 170);
+            this.VodkaPicture.Name = "VodkaPicture";
+            this.VodkaPicture.Size = new System.Drawing.Size(140, 129);
+            this.VodkaPicture.TabIndex = 0;
+            this.VodkaPicture.TabStop = false;
+            this.VodkaPicture.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // VodkaCounter
+            // 
+            this.VodkaCounter.AutoSize = true;
+            this.VodkaCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VodkaCounter.Location = new System.Drawing.Point(96, 82);
+            this.VodkaCounter.Name = "VodkaCounter";
+            this.VodkaCounter.Size = new System.Drawing.Size(124, 25);
+            this.VodkaCounter.TabIndex = 1;
+            this.VodkaCounter.Text = "Vodkas:  0";
+            // 
+            // VodkaButton
+            // 
+            this.VodkaButton.Location = new System.Drawing.Point(438, 305);
+            this.VodkaButton.Name = "VodkaButton";
+            this.VodkaButton.Size = new System.Drawing.Size(140, 94);
+            this.VodkaButton.TabIndex = 2;
+            this.VodkaButton.Text = "Make";
+            this.VodkaButton.UseVisualStyleBackColor = true;
+            this.VodkaButton.Click += new System.EventHandler(this.DrinkButton_Click);
+            // 
+            // ShopDistiller
+            // 
+            this.ShopDistiller.Location = new System.Drawing.Point(761, 165);
+            this.ShopDistiller.Name = "ShopDistiller";
+            this.ShopDistiller.Size = new System.Drawing.Size(75, 23);
+            this.ShopDistiller.TabIndex = 3;
+            this.ShopDistiller.Text = "Distiller";
+            this.ShopDistiller.UseVisualStyleBackColor = true;
+            this.ShopDistiller.Click += new System.EventHandler(this.ShopDistiller_Click);
+            // 
+            // DistillerTimer
+            // 
+            this.DistillerTimer.Interval = 1000;
+            this.DistillerTimer.Tick += new System.EventHandler(this.DistillerTimer_Tick);
+            // 
+            // DistillerCounterLabel
+            // 
+            this.DistillerCounterLabel.AutoSize = true;
+            this.DistillerCounterLabel.Location = new System.Drawing.Point(842, 170);
+            this.DistillerCounterLabel.Name = "DistillerCounterLabel";
+            this.DistillerCounterLabel.Size = new System.Drawing.Size(57, 13);
+            this.DistillerCounterLabel.TabIndex = 4;
+            this.DistillerCounterLabel.Text = "Distillers: 0";
+            // 
+            // DistillerPriceLabel
+            // 
+            this.DistillerPriceLabel.AutoSize = true;
+            this.DistillerPriceLabel.Location = new System.Drawing.Point(778, 197);
+            this.DistillerPriceLabel.Name = "DistillerPriceLabel";
+            this.DistillerPriceLabel.Size = new System.Drawing.Size(49, 13);
+            this.DistillerPriceLabel.TabIndex = 5;
+            this.DistillerPriceLabel.Text = "Price: 50";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.DistillerPriceLabel);
+            this.Controls.Add(this.DistillerCounterLabel);
+            this.Controls.Add(this.ShopDistiller);
+            this.Controls.Add(this.VodkaButton);
+            this.Controls.Add(this.VodkaCounter);
+            this.Controls.Add(this.VodkaPicture);
+            this.Name = "Form1";
+            this.Text = "Vodka Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VodkaPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox VodkaPicture;
+        private System.Windows.Forms.Label VodkaCounter;
+        private System.Windows.Forms.Button VodkaButton;
+        private System.Windows.Forms.Button ShopDistiller;
+        private System.Windows.Forms.Timer DistillerTimer;
+        private System.Windows.Forms.Label DistillerCounterLabel;
+        private System.Windows.Forms.Label DistillerPriceLabel;
+    }
+}
+
---
+++ водка Drinker/водка Drinker/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace водка_Drinker
+{
+    public partial class Form1 : Form
+    {
+        int Vodkas;
+        int MakeStrength = 1;
+        int Distillers = 0;
+        int DistillerPrice = 50;
+        int DistillerSpeed = 2;
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrinkButton_Click(object sender, EventArgs e)
+        {
+            Vodkas = Vodkas + 1;
+            VodkaCounter.Text = "Vodkas: " + Vodkas;
+            if (Vodkas == 5000)
+            {
+                MessageBox.Show("Hello Ma Freind, You Make Very Vodka, I Am Putin, You Good Vodka, You Spetsnaz Vodka, You Win");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShopDistiller_Click(object sender, EventArgs e)
+        {
+            DistillerTimer.Start();
+            if (Vodkas < DistillerPrice)
+            {
+                MessageBox.Show("Not Enough Vodka's");
+                DistillerCounterLabel.Text = "Distillers: " + Distillers;
+            }
+            if (Vodkas > DistillerPrice)
+            {
+                Vodkas = Vodkas - DistillerPrice;
+                DistillerPrice = DistillerPrice * 2;
+                Distillers = Distillers + 1;
+                DistillerPriceLabel.Text = "Price: " + DistillerPrice;
+            }
+        }
+
+        private void DistillerTimer_Tick(object sender, EventArgs e)
+        {
+            Vodkas = Vodkas + Distillers;
+            VodkaCounter.Text = "Vodkas: " + Vodkas;
+            DistillerCounterLabel.Text = "Distillers: " + Distillers;
+        }
+    }
+}
